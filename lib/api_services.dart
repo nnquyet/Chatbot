31700cc77a96bec85671a15894460f65992be9e1
nnquyet <101963206+nnquyet@users.noreply.github.com>
2023-02-28 14:03:27 +0700

    Update api_services.dart

--- lib/api_services.dart
+++ lib/api_services.dart
@@ -4,11 +4,7 @@ import 'dart:math';
 import 'package:http/http.dart' as http;
 
 List<String> myList = [
-  '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',
-  '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',
-  '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',
-  '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',
-  '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',
+  'sk-Fj7XaFV2Zx5cO2TxBh',
 ];
 
 var random = Random();
